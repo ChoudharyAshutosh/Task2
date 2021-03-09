ae2660dc8160e7bb60c8fb02c22e2e666e8b6627
ChoudharyAshutosh <55288012+ChoudharyAshutosh@users.noreply.github.com>
2021-03-09 23:51:27 +0530

    Update task2.js

--- task2.js
+++ task2.js
@@ -4,7 +4,6 @@ var readLine=require("readline").createInterface({
     input: process.stdin,
     output: process.stdout
 })
-const arr=[1,3,5,7,9]
 readLine.question("Enter array : ",array=>{
     var arr=array.split(" ")
     var small=arr[0]
